5b4a4aabb10453222ac60aebc0dfe52dfadf2e26
Pabloyquemas <114018509+Pabloyquemas@users.noreply.github.com>
2022-09-28 16:56:46 +0200

    textview and imagenview

---
+++ app/src/main/java/com/pgonrod/pmdm2022/ut01/ex02/presentation/Ut01Ex02Activity.kt
@@ -0,0 +1,12 @@
+package com.pgonrod.pmdm2022.ut01.ex02.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.pgonrod.pmdm2022.R
+
+class Ut01Ex02Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ut01_ex02)
+    }
+}

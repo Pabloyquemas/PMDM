1acbf6ff2582398dc9d7df7bcaa41916406495c2
Pabloyquemas <114018509+Pabloyquemas@users.noreply.github.com>
2022-10-04 16:20:04 +0200

    Correcion layout ut01ex02b

--- app/src/main/java/com/pgonrod/pmdm2022/ut01/ex02/presentation/Ut01Ex02bActivity.kt
+++ app/src/main/java/com/pgonrod/pmdm2022/ut01/ex02/presentation/Ut01Ex02bActivity.kt
@@ -2,6 +2,7 @@ package com.pgonrod.pmdm2022.ut01.ex02.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.pgonrod.pmdm2022.R
 
 class Ut01Ex02bActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {

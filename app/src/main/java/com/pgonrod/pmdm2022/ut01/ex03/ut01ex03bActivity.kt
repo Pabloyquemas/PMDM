275307b9b3a97a2664869e6c68a436b0d2b7d38a
Pabloyquemas <114018509+Pabloyquemas@users.noreply.github.com>
2022-10-04 17:09:18 +0200

    Relative Layout

---
+++ app/src/main/java/com/pgonrod/pmdm2022/ut01/ex03/ut01ex03bActivity.kt
@@ -0,0 +1,12 @@
+package com.pgonrod.pmdm2022.ut01.ex03
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.pgonrod.pmdm2022.R
+
+class ut01ex03bActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ut01ex03b)
+    }
+}

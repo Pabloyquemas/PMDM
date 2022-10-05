9a2b7c51627e95e194c07bda3b2602b2eed4c1bf
Pabloyquemas <114018509+Pabloyquemas@users.noreply.github.com>
2022-10-05 16:56:33 +0200

    Ex04 CloneToGoodToGo

---
+++ app/src/main/java/com/pgonrod/pmdm2022/ut01/ex04/Ut01Ex04CloneToGoodToGo.kt
@@ -0,0 +1,11 @@
+package com.pgonrod.pmdm2022.ut01.ex04
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Ut01Ex04CloneToGoodToGo : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ut01_ex04_clone_to_good_to_go)
+    }
+}

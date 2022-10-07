4055eb54c56d7e8f08a6c502ccd441a55e7189ae
Pabloyquemas <pabloyquemas2@gmail.com>
2022-10-07 13:46:46 +0200

    Avila en Tapas

---
+++ app/src/main/java/com/pgonrod/pmdm2022/ut01/AvilaEnTapas/Ut01AvilaEnTapasActivity.kt
@@ -0,0 +1,12 @@
+package com.pgonrod.pmdm2022.ut01.AvilaEnTapas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.pgonrod.pmdm2022.R
+
+class Ut01AvilaEnTapasActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ut01_avila_en_tapas)
+    }
+}
